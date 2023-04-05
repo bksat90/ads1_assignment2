33b0e14d142c6a84ef7adb34451131d397a4e0aa
bksat90 <bksat90@gmail.com>
2023-04-05 22:09:10 +0100

    electricity consumption graph

--- assignment.py
+++ assignment.py
@@ -47,11 +47,14 @@ def ElectricityConsumption(df):
     econs_df.drop(['Indicator Name', 'Indicator Code'],
                   axis=1, inplace=True)
     econs_df.reset_index(drop=True, inplace=True)
+    econs_df = econs_df.T
+    econs_df = econs_df.rename(columns=econs_df.iloc[0])
+    econs_df.drop(labels=['Country Name'],axis=0, inplace=True)
     econs_df.rename(columns={'United Kingdom': 'UK',
                              'Russian Federation': 'Russia',
                              'United States': 'US'}, inplace=True)
-    econs_df = econs_df.rename(columns=econs_df.iloc[0])
-    econs_df.drop(labels=['Country Name'],axis=0, inplace=True)
+    econs_df.reset_index(inplace=True)
+    econs_df.drop(econs_df.tail(1).index,inplace=True)
     return econs_df
 
 
@@ -60,5 +63,27 @@ df = pd.read_csv('API_19_DS2_en_csv_v2_4902199.csv', skiprows=4)
 # filters the data for the required countries
 df = FilterData(df)
 df = Preprocess(df)
+edf = ElectricityConsumption(df)
 
 
+
+
+edf['index'] = edf['index'].astype('int')
+edf = edf[edf['index'] > 1994]
+edf = edf[edf['index'] < 2015]
+edf['index']= pd.to_datetime(edf['index'], format='%Y')
+
+#Line plot
+plt.figure(figsize=(8,8), dpi=400)
+plt.plot(edf['index'], edf['China'], label='China')
+plt.plot(edf['index'], edf['Germany'], label='Germany')
+plt.plot(edf['index'], edf['France'], label='France')
+plt.plot(edf['index'], edf['UK'], label='UK')
+plt.plot(edf['index'], edf['India'], label='India')
+plt.plot(edf['index'], edf['Russia'], label='Russia')
+plt.plot(edf['index'], edf['US'], label='US')
+plt.legend()
+plt.xlabel('Years')
+plt.ylabel('kilo Watt hour per capita')
+plt.title('Electric power consumption between 1995 and 2014')
+plt.show()
