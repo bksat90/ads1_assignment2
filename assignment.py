e8f8c51baf1d76c7c38c0fab8e0e3442b378d162
bksat90 <bksat90@gmail.com>
2023-04-05 20:22:48 +0100

    transform function

--- assignment.py
+++ assignment.py
@@ -9,6 +9,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file = pd.read_csv('API_19_DS2_en_csv_v2_4902199.csv', skiprows=4)
 
-file.head(5)
+def transform(df):
+    """
+
+    """
+    #filter out data for US, UK, France, India, China, Australia, Russia
+    countries = ['United States', 'United Kingdom', 'France', 'India', 'China',
+             'Germany', 'Russian Federation', ]
+    df = df.loc[df['Country Name'].isin(countries)]
+    indicator = ['SP.URB.TOTL', 'SP.POP.TOTL', 'SH.DYN.MORT', 'SH.DYN.MORT', 
+                 'ER.H2O.FWTL.K3', 'EN.ATM.GHGT.KT.CE', 'EN.ATM.CO2E.KT',
+                 'EN.ATM.CO2E.SF.KT', 'EN.ATM.CO2E.LF.KT', 'EN.ATM.CO2E.GF.KT',
+                 'EG.USE.ELEC.KH.PC', 'EG.ELC.RNEW.ZS', 'AG.LND.FRST.K2',
+                 'AG.LND.ARBL.ZS', 'AG.LND.AGRI.K2']
+    df = df.loc[df['Indicator Code'].isin(indicator)]
+    return df
+    
+
+df = pd.read_csv('API_19_DS2_en_csv_v2_4902199.csv', skiprows=4)
+
+df.head(5)
+
