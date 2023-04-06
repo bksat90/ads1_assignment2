e6c5f2ee6d6ad2ab4f598b1fe77cde1fee854d9d
bksat90 <bksat90@gmail.com>
2023-04-06 23:59:14 +0100

    pep8

--- assignment.py
+++ assignment.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import cluster_tools as ct
 
+
 def FilterData(df):
     """
     This function filters the dataframe for the required countries such as US,
@@ -18,11 +19,11 @@ def FilterData(df):
     """
     # filter out data for US, UK, France, India, China, Germany, Russia
     countries = ['United States', 'United Kingdom', 'France', 'India', 'China',
-             'Germany', 'Russian Federation', ]
+                 'Germany', 'Russian Federation', ]
     df = df.loc[df['Country Name'].isin(countries)]
-    
+
     # required indicators
-    indicator = ['SP.URB.TOTL', 'SP.POP.TOTL', 'SH.DYN.MORT', 'SH.DYN.MORT', 
+    indicator = ['SP.URB.TOTL', 'SP.POP.TOTL', 'SH.DYN.MORT', 'SH.DYN.MORT',
                  'ER.H2O.FWTL.K3', 'EN.ATM.GHGT.KT.CE', 'EN.ATM.CO2E.KT',
                  'EN.ATM.CO2E.SF.KT', 'EN.ATM.CO2E.LF.KT', 'EN.ATM.CO2E.GF.KT',
                  'EG.USE.ELEC.KH.PC', 'EG.ELC.RNEW.ZS', 'AG.LND.FRST.K2',
@@ -46,21 +47,21 @@ def VariableTrend(df, indicator):
     This function filters the trend for particular indicator
     """
     # filter the given indicator
-    var_df =  df.loc[df['Indicator Code'] == indicator]
+    var_df = df.loc[df['Indicator Code'] == indicator]
     # drops unneccessary columns
     var_df.drop(['Indicator Name', 'Indicator Code'],
-                  axis=1, inplace=True)
+                axis=1, inplace=True)
     var_df.reset_index(drop=True, inplace=True)
     # transpose the dataframe and renamed the column names
     var_df = var_df.T
     var_df = var_df.rename(columns=var_df.iloc[0])
-    var_df.drop(labels=['Country Name'],axis=0, inplace=True)
+    var_df.drop(labels=['Country Name'], axis=0, inplace=True)
     var_df.rename(columns={'United Kingdom': 'UK',
-                             'Russian Federation': 'Russia',
-                             'United States': 'US'}, inplace=True)
+                           'Russian Federation': 'Russia',
+                           'United States': 'US'}, inplace=True)
     var_df.reset_index(inplace=True)
-    var_df.drop(var_df.tail(1).index,inplace=True)
-    
+    var_df.drop(var_df.tail(1).index, inplace=True)
+
     # change the data type of the dataframe to float
     columns = list(var_df.columns)
     for col in columns:
@@ -76,52 +77,50 @@ def HeatmapPreprocess(df, country):
     can be used to generate the heatmap
     """
     hdf = df.loc[df['Country Name'] == country]
-    
+
     # filters required indicators
     indicator = ['SP.URB.TOTL', 'SP.POP.TOTL', 'SH.DYN.MORT', 'ER.H2O.FWTL.K3',
-           'AG.LND.FRST.K2', 'AG.LND.ARBL.ZS', 'AG.LND.AGRI.K2',
-           'EN.ATM.CO2E.KT']
-    hdf =  hdf.loc[hdf['Indicator Code'].isin(indicator)]
-    
+                 'AG.LND.FRST.K2', 'AG.LND.ARBL.ZS', 'AG.LND.AGRI.K2',
+                 'EN.ATM.CO2E.KT']
+    hdf = hdf.loc[hdf['Indicator Code'].isin(indicator)]
+
     # removes the unnecessary columns
     hdf.drop(['Country Name', 'Indicator Code'], axis=1, inplace=True)
     hdf.reset_index(drop=True, inplace=True)
-    
+
     # transpose the data
     hdf = hdf.T
     hdf.reset_index(inplace=True)
     hdf = hdf.rename(columns=hdf.iloc[0])
     hdf.drop(0, inplace=True)
-    
+
     # removes the last three rows as there is no data for those years
-    hdf.drop(hdf.tail(3).index,inplace=True)
-    
+    hdf.drop(hdf.tail(3).index, inplace=True)
+
     # rename the columns names
-    hdf.rename(columns=
-                {'Indicator Name': 'Year',
-                'Population, total': 'Total Population',
-                'Mortality rate, under-5 (per 1,000 live births)':
-                    'Mortality rate',
-                'Annual freshwater withdrawals, total (billion cubic meters)'
-                    : 'Annual freshwater withdrawals',
-                'CO2 emissions (kt)': 'CO2 emissions',
-                'Forest area (sq. km)': 'Forest area',
-                'Arable land (% of land area)': 'Arable land',
-                'Agricultural land (sq. km)': 'Agricultural land'},
-                inplace=True)
-    
+    hdf.rename(columns={'Indicator Name': 'Year',
+                        'Population, total': 'Total Population',
+                        'Mortality rate, under-5 (per 1,000 live births)':
+                        'Mortality rate',
+                        'Annual freshwater withdrawals, total (billion cubic meters)': 'Annual freshwater withdrawals',
+                        'CO2 emissions (kt)': 'CO2 emissions',
+                        'Forest area (sq. km)': 'Forest area',
+                        'Arable land (% of land area)': 'Arable land',
+                        'Agricultural land (sq. km)': 'Agricultural land'},
+               inplace=True)
+
     hdf['Year'] = hdf['Year'].astype('int')
     # keeps data after 1990 as the data is available from 1990
     hdf = hdf[hdf['Year'] >= 1990]
     hdf.reset_index(drop=True, inplace=True)
     hdf['Year'] = hdf['Year'].astype('object')
-    
+
     # type casting of the dataframe columns
     columns = list(hdf.columns)
     for col in columns:
         if col != 'Year':
             hdf[col] = hdf[col].astype('float64')
-    
+
     return hdf
 
 
@@ -132,30 +131,29 @@ def ProcessCO2(df):
     """
     # five years average will be displaced in these years
     years = [1994, 1999, 2004, 2009, 2014, 2019]
-    columns_list = ['index', 'China', 'Germany', 'France', 'UK', 'India', 
+    columns_list = ['index', 'China', 'Germany', 'France', 'UK', 'India',
                     'Russia', 'US']
-    
+
     # creates empty dataframe
     co2_df = pd.DataFrame(columns=columns_list)
-    
+
     # iterates over year to produce the data
     for year in years:
         temp = df[(df['index'] >= (year-4)) & (df['index'] <= year)]
         # arithmetic mean for five years are calculated
         temp_series = temp.mean()
         temp_dict = temp_series.to_dict()
-        co2_df  = co2_df.append(temp_dict, ignore_index=True)
-    
-    
+        co2_df = co2_df.append(temp_dict, ignore_index=True)
+
     co2_df['index'] = co2_df['index'] + 2
     co2_df.rename(columns={'index': 'Year'}, inplace=True)
     co2_df['Year'] = co2_df['Year'].astype('int')
     co2_df['Year'] = co2_df['Year'].astype('object')
-    
+
     co2_df.set_index('Year', inplace=True)
     co2_df = co2_df.T
     co2_df.reset_index(inplace=True)
-    
+
     co2_df.rename(columns={'Year': ''}, inplace=True)
 
     return co2_df
@@ -175,11 +173,11 @@ edf = VariableTrend(df, 'EG.USE.ELEC.KH.PC')
 edf['index'] = edf['index'].astype('int')
 # data is available from 1991 to 2014
 edf = edf[(edf['index'] > 1990) & (edf['index'] < 2015)]
-edf['index']= pd.to_datetime(edf['index'], format='%Y')
+edf['index'] = pd.to_datetime(edf['index'], format='%Y')
 
 # line plot for electricity consumption
 # line growth for various countries
-plt.figure(figsize=(8,8), dpi=500)
+plt.figure(figsize=(8, 8), dpi=500)
 plt.plot(edf['index'], edf['China'], label='China')
 plt.plot(edf['index'], edf['Germany'], label='Germany')
 plt.plot(edf['index'], edf['France'], label='France')
@@ -240,7 +238,7 @@ plt.show()
 pop_df = VariableTrend(df, 'SP.POP.TOTL')
 pop_df.rename(columns={'index': 'Year'}, inplace=True)
 pop_df["Year"] = pop_df['Year'].astype('int')
-pop_df =  pop_df.loc[pop_df['Year'] == 2021]
+pop_df = pop_df.loc[pop_df['Year'] == 2021]
 pop_df.drop(['Year'], axis=1, inplace=True)
 pop_df.reset_index(drop=True, inplace=True)
 pop_df = pop_df.T
@@ -250,7 +248,7 @@ pop_df.rename(columns={0: 'Total Population',
               inplace=True)
 
 # display the graph
-plt.figure( dpi=500)
+plt.figure(dpi=500)
 plt.pie(pop_df['Total Population'], labels=pop_df['Countries'])
 plt.title('Population in the year 2021', fontweight="bold")
 plt.savefig('Population.png')
